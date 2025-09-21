14c9b6bdd7e05516c0e53f0e9190f28fe5b5289d
Aksellercan <akselercan2003@gmail.com>
2025-09-21 17:17:26 +0300

    feat: backspace to cancel choosing

--- src/main/java/com/example/SpotifyWebAPI/Run_Modes/GraphicalInterface/Functions/SceneActions.java
+++ src/main/java/com/example/SpotifyWebAPI/Run_Modes/GraphicalInterface/Functions/SceneActions.java
@@ -5,6 +5,7 @@ import com.example.SpotifyWebAPI.HTTP.HTTPServer;
 import com.example.SpotifyWebAPI.HTTP.SaveHTTPState;
 import com.example.SpotifyWebAPI.Tools.Files.Objects.FileSearch;
 import com.example.SpotifyWebAPI.Tools.Logger.Logger;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -24,12 +25,27 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Shared functions used by JavaFX Controllers
  */
 public final class SceneActions {
+    /**
+     * Saved stage
+     */
     private static Stage currentStage;
+    /**
+     * Default stylesheet to use
+     */
     private static String defaultStylesheet;
+    /**
+     * FXML Files list
+     */
     private static final ArrayList<FileSearch> FXMLPages = new ArrayList<>();
 
     private SceneActions() {}
 
+    /**
+     * Searches inside Resources/Layouts for fxml files and puts it in List
+     */
+    /*
+    Only works when run from IDE at the moment
+     */
     public static void LoadPagesToArray() {
         try {
             URL getFolder = Scene.class.getResource("/Layouts/");
@@ -47,6 +63,9 @@ public final class SceneActions {
         }
     }
 
+    /**
+     * Removes file extension from file
+     */
     private static void removeExtension() {
         StringBuilder sb = new StringBuilder();
         for (FileSearch file : FXMLPages) {
@@ -62,6 +81,10 @@ public final class SceneActions {
         }
     }
 
+    /**
+     * Gets current stage
+     * @return  current stage
+     */
     public static Stage GetCurrentStage() {
         return currentStage;
     }
@@ -144,6 +167,10 @@ public final class SceneActions {
         return FXMLLoader.load(getFXML);
     }
 
+    /**
+     * Stops the background HTTP server thread by changing volatile boolean and interrupting thread
+     * @return  success as boolean
+     */
     public static boolean StopBackgroundHTTPThread() {
         try {
             HTTPServer httpServer = SaveHTTPState.getServer("Fallback");
@@ -158,7 +185,13 @@ public final class SceneActions {
         }
     }
 
+    /**
+     * Search handler, if there are more than 1 result it creates a event handler to choose one of the results. Up to go up the array, down to go down the array, Enter key to choose then change scene and Backspace key to clear search field then stop the event handler to start a new search
+     * @param pageSearchField   JavaFX TextField object
+     * @param searchTerm    Search term
+     */
     public static void SearchTermSelector(TextField pageSearchField, String searchTerm) {
+        Logger.DEBUG.Log("Enter search handler");
         if (searchTerm.isEmpty()) {
             Logger.DEBUG.Log("Search field is empty", false);
             return;
@@ -172,40 +205,62 @@ public final class SceneActions {
             pageSearchField.setText(returnedList.size() + " results");
         }
         if (!returnedList.isEmpty()) {
-            pageSearchField.addEventHandler(KeyEvent.KEY_PRESSED, e -> {
-                switch (e.getCode()) {
-                    case UP:
-                        if (index.get() == returnedList.size()) index.set(0);
-                        if (index.get() == 0) break;
-                        index.set(index.get() - 1);
-                        break;
-                    case DOWN:
-                        if (index.get() == returnedList.size() - 1) break;
-                        index.set(index.get() + 1);
-                        break;
-                    case ENTER:
-                        if (!ignoreFirstEnter.get()) {
-                            ChangeScene(returnedList.get(index.get()));
-                        }
-                        ignoreFirstEnter.set(!ignoreFirstEnter.get());
-                        break;
+            EventHandler<KeyEvent> handler = new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent keyEvent) {
+                    switch (keyEvent.getCode()) {
+                            case UP:
+                                if (index.get() == returnedList.size()) index.set(0);
+                                if (index.get() == 0) break;
+                                index.set(index.get() - 1);
+                                pageSearchField.setText(returnedList.get(index.get()));
+                                Logger.DEBUG.Log("Index = " + index.get() + " Item = " + returnedList.get(index.get()));
+                                break;
+                            case DOWN:
+                                if (index.get() == returnedList.size() - 1) break;
+                                index.set(index.get() + 1);
+                                pageSearchField.setText(returnedList.get(index.get()));
+                                Logger.DEBUG.Log("Index = " + index.get() + " Item = " + returnedList.get(index.get()));
+                                break;
+                            case BACK_SPACE:
+                                Logger.DEBUG.Log("Backspace pressed, leaving handler...", false);
+                                pageSearchField.setText("");
+                                pageSearchField.removeEventHandler(KeyEvent.KEY_PRESSED, this);
+                                return;
+                            case ENTER:
+                                if (!ignoreFirstEnter.get()) {
+                                    Logger.DEBUG.Log("Index = " + index.get() + " Item = " + returnedList.get(index.get()));
+                                    ChangeScene(returnedList.get(index.get()));
+                                    pageSearchField.removeEventHandler(KeyEvent.KEY_PRESSED, this);
+                                }
+                                if (returnedList.size() == 1) {
+                                    ChangeScene(returnedList.get(0));
+                                    pageSearchField.removeEventHandler(KeyEvent.KEY_PRESSED, this);
+                                }
+                                ignoreFirstEnter.set(!ignoreFirstEnter.get());
+                    }
                 }
-                pageSearchField.setText(returnedList.get(index.get()));
-                Logger.DEBUG.Log("Index = " + index.get() + " Item = " + returnedList.get(index.get()));
-            });
-            pageSearchField.removeEventHandler(KeyEvent.KEY_PRESSED, e -> {
-                Logger.INFO.Log("Closed event handler");
-            });
+            };
+            pageSearchField.addEventHandler(KeyEvent.KEY_PRESSED, handler);
             ClearSearch();
         }
     }
 
+    /**
+     * Clears "correctChars" attribute from FileSearch object
+     */
     private static void ClearSearch() {
+        Logger.DEBUG.Log("Clearing search ratings...");
         for (FileSearch fileSearch : FXMLPages) {
             fileSearch.setCorrectChars(0);
         }
     }
 
+    /**
+     * Searches entered term in FileSearch type array by incrementing "correctChars" attribute for Strings that match the characters in search term
+     * @param searchTerm    Search term
+     * @return  List of matching Files as Strings
+     */
     private static List<String> SearchAlgorithm(String searchTerm) {
         searchTerm = searchTerm.toLowerCase();
         List<String> results = new ArrayList<>();
